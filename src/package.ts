7be409c197a1af1c2be6e5aa8836d17826f3eaf0
GlenCFL <glencfl@gmail.com>
2017-12-02 22:06:45 -0500

    Prefer AtomIDE over Linter. Add the activation hook.

--- src/package.ts
+++ src/package.ts
@@ -12,18 +12,28 @@ import { ValidationData } from "./validation-data";
 
 interface PackageState {}
 
-function getSuggestionsStub() {
-  return [];
-}
-
-function onDidInsertSuggestionStub() {
-  return;
+/**
+ * Adjusts the grammar defaults to be more suitable for item filters.
+ * It's not uncommon for item filters to have lines containing thousands of
+ * characters with hundreds of tokens.
+ */
+function adjustGrammarDefaults() {
+  const grammar = atom.grammars.grammarForScopeName("source.filter");
+  if (grammar) {
+    grammar.maxLineLength = 5000;
+    grammar.maxTokensPerLine = 300;
+  }
 }
 
 export class AtomPackage {
-  private subscriptions: CompositeDisposable;
-  private linterDelegate: LinterDelegate;
   private autocompleteProvider: AutocompleteProvider;
+  private linterDelegate: LinterDelegate;
+  private subscriptions: CompositeDisposable;
+  private readonly autocompleteStubs = {
+    onDidInsertSuggestion: () => undefined,
+    getSuggestions: () => [],
+    dispose: () => undefined,
+  };
 
   constructor() {
     this.autocompleteProvider = {
@@ -31,13 +41,21 @@ export class AtomPackage {
       disableForSelector: ".source.filter .comment",
       inclusionPriority: 1,
       excludeLowerPriority: true,
-      getSuggestions: getSuggestionsStub,
-      onDidInsertSuggestion: onDidInsertSuggestionStub,
-      dispose: () => undefined,
+      getSuggestions: this.autocompleteStubs.getSuggestions,
+      onDidInsertSuggestion: this.autocompleteStubs.onDidInsertSuggestion,
+      dispose: this.autocompleteStubs.dispose,
     };
   }
 
-  activate(_: PackageState) {
+  async activate(_: PackageState) {
+    adjustGrammarDefaults();
+
+    const packages = atom.packages.getAvailablePackageNames();
+    if (!(packages.includes("atom-ide-ui") || packages.includes("linter"))) {
+      const deps = await import("atom-package-deps");
+      await deps.install("path-of-exile-item-filter", true);
+    }
+
     this.subscriptions = new CompositeDisposable();
     const validationData = new ValidationData();
     const suggestionData = new SuggestionData();
@@ -48,6 +66,7 @@ export class AtomPackage {
         .bind(completionProvider);
     this.autocompleteProvider.onDidInsertSuggestion = completionProvider
         .onDidInsertSuggestion.bind(completionProvider);
+    this.autocompleteProvider.dispose = completionProvider.dispose.bind(completionProvider);
     // tslint:enable:no-unsafe-any
 
     this.subscriptions.add(
@@ -61,8 +80,9 @@ export class AtomPackage {
     this.subscriptions.dispose();
     if (this.linterDelegate) this.linterDelegate.dispose();
 
-    this.autocompleteProvider.getSuggestions = getSuggestionsStub;
-    this.autocompleteProvider.onDidInsertSuggestion = onDidInsertSuggestionStub;
+    this.autocompleteProvider.getSuggestions = this.autocompleteStubs.getSuggestions;
+    this.autocompleteProvider.onDidInsertSuggestion = this.autocompleteStubs.onDidInsertSuggestion;
+    this.autocompleteProvider.dispose = this.autocompleteStubs.dispose;
   }
 
   serialize(): PackageState {
